e798125a9a83edb17f40312959d6624388b187db
Spoorthy Reddy Vallem <99735348+spoorthyvallem@users.noreply.github.com>
2022-12-22 11:56:25 +0530

    Stopwatch

--- script.js
+++ script.js
@@ -1,35 +1,78 @@
-(function () {
-    const second = 1000,
-    minute = second * 60,
-    hour = minute * 60,
-    day = hour * 24;
-    let today = new Date(),
-    dd = String(today.getDate()).padStart(2, "0"),
-    mm = String(today.getMonth() + 1).padStart(2, "0"),
-    yyyy = today.getFullYear(),
-    nextYear = yyyy + 1,
-    dayMonth = "10/08/",
-    birthday = dayMonth + yyyy;
-    today = mm + "/" + dd + "/" + yyyy;
-    if (today > birthday) {
-    birthday = dayMonth + nextYear;
-    }
-    //end
-    const countDown = new Date(birthday).getTime(),
-    x = setInterval(function() {
-    const now = new Date().getTime(),
-    distance = countDown - now;
-    document.getElementById("days").innerText = Math.floor(distance / (day)),
-    document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-    document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-    document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-    //do something later when date is reached
-    if (distance < 0) {
-    document.getElementById("headline").innerText = "It's my birthday!";
-    document.getElementById("countdown").style.display = "none";
-    document.getElementById("content").style.display = "block";
-    clearInterval(x);
-    }
-    //seconds
-    }, 0)
-    }());
+let startBtn = document.getElementById('start');
+let stopBtn = document.getElementById('stop');
+let resetBtn = document.getElementById('reset');
+
+let hour = 00;
+let minute = 00;
+let second = 00;
+let count = 00;
+
+startBtn.addEventListener('click', function () {
+	timer = true;
+	stopWatch();
+});
+
+stopBtn.addEventListener('click', function () {
+	timer = false;
+});
+
+resetBtn.addEventListener('click', function () {
+	timer = false;
+	hour = 0;
+	minute = 0;
+	second = 0;
+	count = 0;
+	document.getElementById('hr').innerHTML = "00";
+	document.getElementById('min').innerHTML = "00";
+	document.getElementById('sec').innerHTML = "00";
+	document.getElementById('count').innerHTML = "00";
+});
+
+function stopWatch() {
+	if (timer) {
+		count++;
+
+		if (count == 100) {
+			second++;
+			count = 0;
+		}
+
+		if (second == 60) {
+			minute++;
+			second = 0;
+		}
+
+		if (minute == 60) {
+			hour++;
+			minute = 0;
+			second = 0;
+		}
+
+		let hrString = hour;
+		let minString = minute;
+		let secString = second;
+		let countString = count;
+
+		if (hour < 10) {
+			hrString = "0" + hrString;
+		}
+
+		if (minute < 10) {
+			minString = "0" + minString;
+		}
+
+		if (second < 10) {
+			secString = "0" + secString;
+		}
+
+		if (count < 10) {
+			countString = "0" + countString;
+		}
+
+		document.getElementById('hr').innerHTML = hrString;
+		document.getElementById('min').innerHTML = minString;
+		document.getElementById('sec').innerHTML = secString;
+		document.getElementById('count').innerHTML = countString;
+		setTimeout(stopWatch, 10);
+	}
+}
