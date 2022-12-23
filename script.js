a48b84f715d8679cf198712a9415e0c5172080f5
Spoorthy Reddy Vallem <99735348+spoorthyvallem@users.noreply.github.com>
2022-12-23 15:51:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,78 +1,55 @@
-let startBtn = document.getElementById('start');
-let stopBtn = document.getElementById('stop');
-let resetBtn = document.getElementById('reset');
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
 
-let hour = 00;
-let minute = 00;
-let second = 00;
-let count = 00;
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
 
-startBtn.addEventListener('click', function () {
-	timer = true;
-	stopWatch();
-});
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 
-stopBtn.addEventListener('click', function () {
-	timer = false;
-});
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
 
-resetBtn.addEventListener('click', function () {
-	timer = false;
-	hour = 0;
-	minute = 0;
-	second = 0;
-	count = 0;
-	document.getElementById('hr').innerHTML = "00";
-	document.getElementById('min').innerHTML = "00";
-	document.getElementById('sec').innerHTML = "00";
-	document.getElementById('count').innerHTML = "00";
-});
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
 
-function stopWatch() {
-	if (timer) {
-		count++;
-
-		if (count == 100) {
-			second++;
-			count = 0;
-		}
-
-		if (second == 60) {
-			minute++;
-			second = 0;
-		}
-
-		if (minute == 60) {
-			hour++;
-			minute = 0;
-			second = 0;
-		}
-
-		let hrString = hour;
-		let minString = minute;
-		let secString = second;
-		let countString = count;
-
-		if (hour < 10) {
-			hrString = "0" + hrString;
-		}
-
-		if (minute < 10) {
-			minString = "0" + minString;
-		}
-
-		if (second < 10) {
-			secString = "0" + secString;
-		}
-
-		if (count < 10) {
-			countString = "0" + countString;
-		}
-
-		document.getElementById('hr').innerHTML = hrString;
-		document.getElementById('min').innerHTML = minString;
-		document.getElementById('sec').innerHTML = secString;
-		document.getElementById('count').innerHTML = countString;
-		setTimeout(stopWatch, 10);
-	}
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
